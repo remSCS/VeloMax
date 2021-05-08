8a24583e81ddf709b47afdb5fcac45c22ea533b4
Rémi LOMBARD <remi.lombard@edu.devinci.fr>
2021-05-09 00:26:05 +0200

    Création de la classe qui gère les données

---
+++ ApplicationVeloMax/Communication/DataAccess.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using ApplicationVeloMax.Models;
+using MySql.Data.MySqlClient;
+
+namespace ApplicationVeloMax.Communication
+{
+    public class DataAccess
+    {
+        static private string _connectionString;
+        public DataAccess(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        static MySqlConnection GetConnection()
+        {
+            try
+            {
+                return new MySqlConnection(_connectionString);
+            }
+            catch (MySqlException e)
+            {
+                MessageBox.Show("Connection Error : " + e.ToString());
+            }
+            return new MySqlConnection(_connectionString);
+        }
+
+        static public void GetAllGrandeurs()
+        {
+            using (var connexion = GetConnection())
+            {
+                DataTable dt = new DataTable();
+                MySqlCommand com = new MySqlCommand("select * from grandeur;", connexion);
+                MySqlDataAdapter da = new MySqlDataAdapter(com);
+                da.Fill(dt);
+
+                List<Grandeur> grandeurs = new List<Grandeur>();
+                foreach (DataRow i in dt.Rows)
+                {
+                    grandeurs.Add(new Grandeur()
+                    {
+                        Id = i.Field<int>("idGrandeur"),
+                        Nom = i.Field<string>("nomGrandeur")
+                    });
+                }
+            }
+        }
+
+        static public void GetAllLigneProduits()
+        {
+            using (var connexion = GetConnection())
+            {
+                DataTable dt = new DataTable();
+                MySqlCommand com = new MySqlCommand("select * from LigneProduit;", connexion);
+                MySqlDataAdapter da = new MySqlDataAdapter(com);
+                da.Fill(dt);
+
+                List<LigneProduit> lignesProduits = new List<LigneProduit>();
+
+                foreach (DataRow i in dt.Rows)
+                {
+                    lignesProduits.Add(new LigneProduit()
+                    {
+                        Id = i.Field<int>("idLigneProduit"),
+                        Nom = i.Field<string>("nomLigneProduit")
+                    });
+                }
+            }
+        }
+
+        static public void GetAllModels()
+        {
+            using (var connexion = GetConnection())
+            {
+                DataTable dt = new DataTable();
+                MySqlCommand com = new MySqlCommand("select * from modele;", connexion);
+                MySqlDataAdapter da = new MySqlDataAdapter(com);
+                da.Fill(dt);
+
+                List<Modele> modeles = new List<Modele>();
+
+                Console.WriteLine("NOmbre de grandeurs : " + Grandeur.ensembleGrandeurs.Count());
+
+                foreach (DataRow i in dt.Rows)
+                {
+                    modeles.Add(new Modele(i.Field<int>("idGrandeur"), i.Field<int>("idLigneProduit"))
+                    {
+                        Id = i.Field<int>("idModele"),
+                        Nom = i.Field<string>("nomModele"),
+                        PrixUnitaire = i.Field<decimal>("prixUnitaireModele"),
+                        Quantite = i.Field<int>("quantiteModele"),
+                        DateE = i.Field<DateTime>("dateEModele"),
+                        DateS = i.Field<DateTime>("dateSModele"),
+                    });
+                    Console.WriteLine();
+                }
+
+                foreach (var m in modeles) Console.WriteLine(m);
+            }
+        }
+    }
+}
--- ApplicationVeloMax/MainWindow.xaml.cs
+++ ApplicationVeloMax/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using MySql.Data.MySqlClient;
 using System.Data;
 using ApplicationVeloMax.Models;
+using ApplicationVeloMax.Communication;
 
 namespace ApplicationVeloMax
 {
@@ -24,29 +25,11 @@ namespace ApplicationVeloMax
         {
             InitializeComponent();
 
-            #region Basic Initialization
-            MySqlConnection cnn = null;
-            try
-            {
-                string cs = "SERVER=84.102.235.128;PORT=3306;DATABASE=velomax;UID=RemoteAdmin;PASSWORD=Password@123";
-                cnn = new MySqlConnection(cs);
-                cnn.Open();
-            }
-            catch (MySqlException e)
-            {
-                MessageBox.Show("Connection Error : " + e.ToString());
-                return;
-            }
-            MessageBox.Show("Connected successfully");
-            #endregion
-
-            GetAllGrandeurs(cnn);
-            GetAllLigneProduits(cnn);
-            GetAllModels(cnn);
-            cnn.Close();
+            DataAccess cs = new DataAccess("SERVER=84.102.235.128;PORT=3306;DATABASE=velomax;UID=RemoteAdmin;PASSWORD=Password@123");
+            DataAccess.GetAllGrandeurs();
+            DataAccess.GetAllLigneProduits();
+            DataAccess.GetAllModels();
         }
-
-
 
         static void sqlRequest(MySqlConnection cnn, string request, List<MySqlParameter> parameters = null)
         {
@@ -73,73 +56,6 @@ namespace ApplicationVeloMax
 
             reader.Close();
             cmd.Dispose();
-        }
-
-        static void GetAllGrandeurs(MySqlConnection connexion)
-        {
-            DataTable dt = new DataTable();
-            MySqlCommand com = new MySqlCommand("select * from grandeur;", connexion);
-            MySqlDataAdapter da = new MySqlDataAdapter(com);
-            da.Fill(dt);
-
-            List<Grandeur> grandeurs = new List<Grandeur>();
-            foreach (DataRow i in dt.Rows)
-            {
-                grandeurs.Add(new Grandeur()
-                {
-                    Id = i.Field<int>("idGrandeur"),
-                    Nom = i.Field<string>("nomGrandeur")
-                });
-            }
-            MessageBox.Show("Grandeur done");
-        }
-
-        static void GetAllLigneProduits(MySqlConnection connexion) 
-        {
-            DataTable dt = new DataTable();
-            MySqlCommand com = new MySqlCommand("select * from LigneProduit;", connexion);
-            MySqlDataAdapter da = new MySqlDataAdapter(com);
-            da.Fill(dt);
-
-            List<LigneProduit> lignesProduits = new List<LigneProduit>();
-
-            foreach (DataRow i in dt.Rows)
-            {
-                lignesProduits.Add(new LigneProduit()
-                {
-                    Id = i.Field<int>("idLigneProduit"),
-                    Nom = i.Field<string>("nomLigneProduit")
-                });
-            }
-            MessageBox.Show("Ligne Produit done");
-        }
-
-        static void GetAllModels(MySqlConnection connexion)
-        {
-            DataTable dt = new DataTable();
-            MySqlCommand com = new MySqlCommand("select * from modele;", connexion);
-            MySqlDataAdapter da = new MySqlDataAdapter(com);
-            da.Fill(dt);
-
-            List<Modele> modeles = new List<Modele>();
-
-            Console.WriteLine("NOmbre de grandeurs : " + Grandeur.ensembleGrandeurs.Count());
-
-            foreach (DataRow i in dt.Rows)
-            {
-                modeles.Add(new Modele(i.Field<int>("idGrandeur"), i.Field<int>("idLigneProduit"))
-                {
-                    Id = i.Field<int>("idModele"),
-                    Nom = i.Field<string>("nomModele"),
-                    PrixUnitaire = i.Field<decimal>("prixUnitaireModele"),
-                    Quantite = i.Field<int>("quantiteModele"),
-                    DateE = i.Field<DateTime>("dateEModele"),
-                    DateS = i.Field<DateTime>("dateSModele"),
-                });
-                Console.WriteLine();
-            }
-
-            foreach (var m in modeles) Console.WriteLine(m);
         }
     }
 }
